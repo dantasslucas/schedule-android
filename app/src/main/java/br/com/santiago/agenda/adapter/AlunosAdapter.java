ce4d30061d5a5114e3e43c5d6d01e82f844966ef
Lucas Santiago Dantas <dantas.lucas1994@gmail.com>
2018-02-25 14:40:15 -0300

    Melhorando visual da lista layout/list_item.xml

---
+++ app/src/main/java/br/com/santiago/agenda/adapter/AlunosAdapter.java
@@ -0,0 +1,31 @@
+package br.com.santiago.agenda.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by lucas-santiago on 25/02/18.
+ */
+
+public class AlunosAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
